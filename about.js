7c66a7426da86733a43660e7014697e74f5e2ccc
Shazeen Fabius <Shazeen-Fabius@ShazeenabiusMBP.lan>
2020-10-11 21:35:57 -0700

    changing blog page to about me page

--- about.js
+++ about.js
@@ -1,18 +1,13 @@
 let par1 = document.querySelector('.par1');
 let par2 = document.querySelector('.par2');
-let par3 = document.querySelector('.par3');
 let hr1 = document.querySelector('.hr1');
 let hr2 = document.querySelector('.hr2');
-let hr3 = document.querySelector('.hr3');
 
 let button = document.querySelector('button')
 
 button.addEventListener('click', (event) => {
     par1.classList.toggle('expand');
     par2.classList.toggle('expand');
-    par3.classList.toggle('expand');
     hr1.classList.toggle('expand');
     hr2.classList.toggle('expand');
-    hr3.classList.toggle('expand');
-
 })
